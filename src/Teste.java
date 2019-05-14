ac8b30b7cada03071501526f8b3bb34502f7399f
TheCanT <goncalo.nuno.ferreira.1999@gmail.com>
2019-05-14 17:26:47 +0100

    org 2

--- src/Teste.java
+++ src/Teste.java
@@ -2,14 +2,9 @@ public class Teste {
     public static void main(String[] args){
 
 
-<<<<<<< HEAD
         CatClientes c = new CatClientes();
         c.readClientes("Clientes.txt");
-=======
-        CatVendas c = new CatVendas();
-        //c.readVendas("Vendas_1M.txt");
-        
->>>>>>> 43a3c612d3f3113587923d3911be2fafc57ece1f
+
         System.out.println(c);
 
     }
