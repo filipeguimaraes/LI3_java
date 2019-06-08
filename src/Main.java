fdb5f62fc47966067a6631f34abadf068b925ec5
TheCanT <goncalo.nuno.ferreira.1999@gmail.com>
2019-06-08 09:33:07 +0100

    Ajuste das interfaces IGereVendasModel

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,8 @@
 public class Main {
-/*    public static List<String> readFilesWithNIO(String filePath){
-        Path p = Paths.get(filePath);
-        List<String> l = null;
-        try{
-            l= Files.readAllLines(p, StandardCharsets.UTF_8);
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        return l;
-    }*/
-
-/*
-    public static List<String> readLinesWithBuff(String fich){
-        List<String> linhas = new ArrayList<>();
-        String linha = null;
-
-        try(
-            BufferedReader inStream = new BufferedReader(new FileReader(fich))){
-                while((linha= inStream.readLine())!=null){
-                ////// if linha.metodo verifica-se
-                linhas.add(linha);
-                }
-            }
-        catch(IOException e){
-        System.out.println(e);
-        }
-        return linhas;
-    }
-
- */
 
     public static void main(String[] args){
         //GereVendasModel model = createData();
-        GereVendasModel model = new GereVendasModel();
+        IGereVendasModel model = new GereVendasModel();
         /*
         if(model == null) {
             System.out.println("ERRO INICIALIZACAO");  // throw exception ?
